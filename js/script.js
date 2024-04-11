144c8b5911ac4e1162b060f2d3c45c1c9d3234ad
shparknr <shparknr11@naver.com>
2024-04-11 11:01:46 +0900

    while 문

--- js/script.js
+++ js/script.js
@@ -1,20 +1,5 @@
-// 이중 for 문
+let x = -10;
 
-// 2단
-let x = 2;
-let result;
-
-for (let y = 1; y < 10; y++) {
-  result = x * y;
-  console.log(result);
-}
-
-// 구구단
-let resultAll;
-
-for (let x = 2; x < 10; x++) {
-  for (let y = 1; y < 10; y++) {
-    resultAll = x * y;
-    console.log(resultAll);
-  }
-}
+do {
+  console.log("안녕!");
+} while (x > 0);
